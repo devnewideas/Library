283f6c78f998a01c3c7cfad62100cd397e4f27d7
Prasanna Venkatesh <devnewideas@gmail.com>
2021-06-05 21:32:16 +0200

    Code checkin with Model class files

---
+++ Library.Models/Book.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="Book.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Library.Models
+{
+    using System;
+
+    /// <summary>
+    /// This Model class is used to declare the properties for book.
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Gets or sets identity Value.
+        /// </summary>
+        /// <type>int.</type>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title of the book.
+        /// </summary>
+        /// <type>string.</type>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the international standard book number.
+        /// </summary>
+        /// <type>string.</type>
+        public string ISBN { get; set; }
+
+        /// <summary>
+        /// Gets or sets the checkout date of book.
+        /// </summary>
+        /// <type>DateTime.</type>
+        public DateTime? CheckOutDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Reader Object.
+        /// </summary>
+        /// <type>Reader.</type>
+        public Reader Reader { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether gets or sets a flag value.
+        /// </summary>
+        /// <type>bool.</type>
+        public bool Lost { get; set; }
+    }
+}
---
+++ Library.Models/Reader.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Reader.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Library.Models
+{
+    using System;
+
+    /// <summary>
+    /// This Model class is used to declare the properties for reader.
+    /// </summary>
+    public class Reader
+    {
+        /// <summary>
+        /// Gets or sets identity Value.
+        /// </summary>
+        /// <type>int.</type>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets name of the reader.
+        /// </summary>
+        /// <type>string.</type>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets date of birth.
+        /// </summary>
+        /// <type>DateTime.</type>
+        public DateTime DOB { get; set; }
+    }
+}
